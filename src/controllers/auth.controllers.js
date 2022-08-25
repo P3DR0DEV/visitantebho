1a8016de314a968706f749e4b9665e5400766445
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-08-25 13:26:04 -0300

    .

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,27 +1,23 @@
 const ad = require('../config/activeDirectory.js')
 require('dotenv').config()
 const domain = process.env.DOMAIN
+const permissoes = [{ username:'' }, { username:'' }]
 
 //Método para autenticar usuários
 exports.user_authenticate = async (req, res) => {
   const { user, pass } = req.body;
-  // console.log(user)
-  // console.log(pass)
-  // console.log(domain)
-  // const query = 'CN=BHO'
   try {
     await ad.authenticate( domain + "\\" + user, pass,
     function (err, auth) {
       if (auth) {
-        // ad.findGroups(query,(err, groups)=>{
-        //   if(err){
-        //     console.log("ERRO:", err)
-        //   }
-        //   console.log(groups)
-        // })
-        return res.status(200).json({
-          message: "Authenticated!"
-        });
+        // return res.status(200).json({
+          // message: "Authenticated!"
+        // });
+        const valid = permissoes.find(allowedUsers => allowedUsers.username === user)
+        if (!valid){
+          return res.sendStatus(403)
+        }
+        return res.sendStatus(200)
       }
       else {
         return res.status(401).send({
@@ -29,13 +25,6 @@ exports.user_authenticate = async (req, res) => {
           error: err
         });
       }
-    });
-    ad.groupExists('CTIC-BHO',(err, result)=>{
-      // if(err){
-      //   console.log(err)
-      //   return
-      // };
-      console.log(result)
     })
   }catch (err) {
     return res.status(500).send({ message: "ERROR " + err });
