8f14c8384adf872b5f9918d2a82b13d81cb3d53c
rotrenanoliveira <rotrenanoliveira@gmail.com>
2022-08-26 11:08:47 -0300

    removendo arquivos para total refatoração

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require(__dirname + '/src/app')
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, ()=>{
-    console.log('Listening to port: ', port);
-});
--- src/routers/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express')();
-const authController = require('../controllers/auth.controllers');
-
-// => Definindo as rotas de autenticação
-
-router.post('/login', authController.user_authenticate); //{POST} localhost:3000/api/ad/login
-
-module.exports = router;
