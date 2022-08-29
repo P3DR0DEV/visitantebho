c5f388818988c07b37bfd5da1a088d047ea80aa1
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-08-29 17:48:09 -0300

    Tentando me comunicar de fora do localhost

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const mongoDb = process.env.MONGODB_URI
 const port = process.env.PORT || 3000;
 
 mongoose.connect(mongoDb)
- .then(app.listen(port, ()=>{
-    console.log('Listening to port: ', port);
+ .then(app.listen(port,'0.0.0.0', ()=>{ 
+   console.log('Listening to port: ', port);
  }))
  .catch(e => {
     console.log('ERRO: ', e)
  });
+ 
