ddf9a657283e7cc43a778581004cbc2388ccb400
Pedro Henrique Campos Mendes <Pedpehenriquecamposmendes@hotmail.com>
2022-09-01 21:27:39 -0300

    add rout post logout

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,10 +1,15 @@
 const router = require('express')();
 const authController = require('../controllers/auth.controllers');
-
 // => Definindo as rotas de autenticação
 
 router.post('/login', authController.user_authenticate); //{POST} localhost:3000/api/ad/login
 router.get('/login', (req, res)=>{
     res.redirect('/')
+});
+router.post('/logout',(req, res)=>{ 
+    res.clearCookie('refreshtoken', { path:'/refresh_token' })
+    res.send({
+        message:'Logged out'
+    })
 })
 module.exports = router;
