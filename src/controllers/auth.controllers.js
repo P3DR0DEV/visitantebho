f4d6b62b02275f3d27846be07fe929155938b034
Pedro Henrique Campos Mendes <Pedpehenriquecamposmendes@hotmail.com>
2022-09-01 21:26:29 -0300

    add token creation on login

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,23 +1,28 @@
 const ad = require('../config/activeDirectory.js');
 require('dotenv').config();
 const domain = process.env.DOMAIN;
-const { genTokens } = require('../config/token/genNewToken')
+const { 
+  genTokens,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken 
+} = require('../config/token/genNewToken')
 const permissoes = [{ username:'150367' }, { username:'150176' }];
 
 //Método para autenticar usuários
 exports.user_authenticate = async (req, res) => {
   const { user, pass } = req.body;
   try {
-    await ad.authenticate( domain + "\\" + user, pass,
-    function (err, auth) {
+    await ad.authenticate( domain + "\\" + user, pass,(err, auth) => {
       if (auth) {
         const valid = permissoes.find(allowedUsers => allowedUsers.username === user)
         if (!valid){
           return res.status(403).redirect('/notAllowed')
         }
-        const token = genTokens({ user });
-        req.headers['Authorization'] = `Bearer ${token}`
-        
+        const accessToken = genTokens({ user });
+        const refreshToken = createRefreshToken({ user });
+        sendAccessToken(req, res, accessToken);
+        sendRefreshToken(res, refreshToken) 
       }
       else {
         return res.status(401).send({
