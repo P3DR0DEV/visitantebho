598b3cd0ad5b0a153a75461ead9ae8fe1f270b91
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-09-20 16:23:27 -0300

    Update logged view & error messages

--- src/middlewares/loginErros.js
+++ src/middlewares/loginErros.js
@@ -1,5 +1,6 @@
 const erros = (req,res,next)=>{
     res.locals.errors = req.flash('errors');
+    res.locals.user = req.session.user;
     next()
 }
 
--- src/middlewares/validUser.js
+++ src/middlewares/validUser.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 function validUser(req, res, next) {
     try {
         console.log(req.session.user)
-        if((!req.session.user.user && req.session.user.logado !== true)){
+        if(!req.session.user){
             res.redirect('/notAllowed')
         } else{
             next()
